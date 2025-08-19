d772f5609ae65479c689a1cd761e709244d3f4df
Keshav <munjabhaukhodke@gmail.com>
2025-08-19 14:10:03 +0530

    Initial commit

--- Second_Day_19_08_25/Second.java
+++
@@ -1,8 +0,0 @@
-package Second_Day_19_08_25;
-
-public class Second {
-
-    public static void main(String[] args) {
-        System.out.println("second day ");
-    }
-}
---
+++ variables_ Second_Day_19_08_25/Variables_Demo.java
@@ -0,0 +1,16 @@
+
+class Variables_Demo
+{
+    public static void main(String[] args) {
+        byte x=10;
+        System.out.println(x);
+    }
+}
+
+// the rule to create and implement the variabe is
+
+// type  varable = data;
+// while priting the variable we cannot use ""
+// we use double quotes only when we want to display a message or senstences or words.
+
+
